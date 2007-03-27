ae020e980036f1b303bf0328ca7783da2b5ae4bf
Darren Kulp <kulp@cpan.org>
2007-03-27 02:55:19 +0000

    Import of KULP/Text-vFile-toXML-0.02 from CPAN.
    gitpan-cpan-distribution: Text-vFile-toXML
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         KULP/Text-vFile-toXML-0.02.tar.gz
    gitpan-cpan-author:       KULP
    gitpan-cpan-maturity:     released

--- lib/Text/vFile/toXML.pm
+++ lib/Text/vFile/toXML.pm
@@ -9,11 +9,11 @@ Text::vFile::toXML - Convert vFiles into equivalent XML
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Carp qw(croak);
 use Text::vFile::asData;
